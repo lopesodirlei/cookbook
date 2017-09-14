1e8bb689c6756b77b6b14cb33659a2d10c27c682
aluno <aluno@juridico03>
2017-09-14 20:29:34 -0300

    Criando as classes de negócio receita e ingrediente.

---
+++ cookbook/src/br/rj/macae/femass/cookbook/modelo/Ingrediente.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rj.macae.femass.cookbook.modelo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Ingrediente {
+    private String nome;
+    
+}
---
+++ cookbook/src/br/rj/macae/femass/cookbook/modelo/Receita.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rj.macae.femass.cookbook.modelo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Receita {
+    
+    private int id;
+    private String nome;
+    private String ingredientes;
+    private String modoDePreparo;
+    private String modoDeServir;
+
+    
+}
